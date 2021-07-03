8b98a54f7e54b6edeff29de838f8fb048d35e1d8
“hhy” <1352963143@qq.com>
2021-07-03 22:03:31 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.jiaofb.test;
+
+/**
+ * @Auther: iteration.0
+ * @Date: 3/7/2021 - 07 - 03 - 9:41 PM
+ * @Description: com.jiaofb.test
+ * @version: 1.0
+ */
+public class HelloWorld {
+    //这是 一个main方法，是程序的入口：
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
